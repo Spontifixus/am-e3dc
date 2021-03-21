9879b55bc7b1b4fc991efb3ec95254b87529b49a
Markus Rehrs <markus.rehrs@zuehlke.com>
2021-03-21 22:20:44 +0100

    Updated example program

--- Source/AM.E3dc.Rscp.Example/Program.cs
+++ Source/AM.E3dc.Rscp.Example/Program.cs
@@ -1,54 +1,41 @@
-﻿using System.Net;
-using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using AM.E3dc.Rscp;
 using AM.E3dc.Rscp.Data;
 using AM.E3dc.Rscp.Data.Values;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
 
-namespace AM.E3dc.Rscp.Example
+const string e3dcIpAddress = "192.168.0.2";
+const string e3dcUserName = "your-e3dc-portal-username@example.com";
+const string e3dcPassword = "your-e3dc-portal-password";
+const string rscpPassword = "your-rscp-password";
+
+using var loggerFactory = LoggerFactory.Create(builder =>
 {
-    /// <summary>
-    /// Ja blah.
-    /// </summary>
-    public class Program
-    {
-        /// <summary>
-        /// Genau.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-        public static async Task Main()
-        {
-            using var loggerFactory = LoggerFactory.Create(builder =>
-                {
-                    builder.AddSimpleConsole(
-                        o =>
-                        {
-                            o.TimestampFormat = "dd HH: mm:ss.fff ";
-                            o.ColorBehavior = LoggerColorBehavior.Disabled;
-                            o.SingleLine = true;
-                        });
+    builder.AddSimpleConsole(c => c.SingleLine = true);
+    builder.SetMinimumLevel(LogLevel.Trace);
+});
 
-                    builder.SetMinimumLevel(LogLevel.Trace);
-                });
+var e3dcLogger = loggerFactory.CreateLogger<E3dcConnection>();
+var logger = loggerFactory.CreateLogger("Program");
+logger.LogInformation("Logging configured.");
 
-            var logger = loggerFactory.CreateLogger<E3dcConnection>();
+logger.LogInformation("Setting up connection to E3/DC power station...");
+var e3dcConnection = new E3dcConnection(e3dcLogger);
+var endpoint = new IPEndPoint(IPAddress.Parse(e3dcIpAddress), 5033);
+await e3dcConnection.ConnectAsync(endpoint, rscpPassword);
 
-            var e3dcConnection = new E3dcConnection(logger);
+logger.LogInformation("Starting authorization for user '{userName}'...", e3dcUserName);
+var authFrame = new RscpFrame();
+var authContainer = new RscpContainer(RscpTag.TAG_RSCP_REQ_AUTHENTICATION);
+authContainer.Add(new RscpString(RscpTag.TAG_RSCP_AUTHENTICATION_USER, e3dcUserName));
+authContainer.Add(new RscpString(RscpTag.TAG_RSCP_AUTHENTICATION_PASSWORD, e3dcPassword));
+authFrame.Add(authContainer);
 
-            var endpoint = new IPEndPoint(IPAddress.Parse("192.168.0.2"), 5033);
-            await e3dcConnection.ConnectAsync(endpoint, "your-rscp-password");
+var response = await e3dcConnection.SendAsync(authFrame);
+response.TryGetValue<RscpUInt8>(RscpTag.TAG_RSCP_AUTHENTICATION, out var authResponse);
 
-            var authFrame = new RscpFrame();
-            var authContainer = new RscpContainer(RscpTag.TAG_RSCP_REQ_AUTHENTICATION);
-            authContainer.Add(new RscpString(RscpTag.TAG_RSCP_AUTHENTICATION_USER, "your-portal-username@example.com"));
-            authContainer.Add(new RscpString(RscpTag.TAG_RSCP_AUTHENTICATION_PASSWORD, "your-portal-password"));
-            authFrame.Add(authContainer);
+var userLevel = (RscpUserLevel)authResponse.Value;
+logger.LogInformation("Authorization of '{userName}' successful (UserLevel: {userLevel}).", e3dcUserName, userLevel);
 
-            var response = await e3dcConnection.SendAsync(authFrame);
-
-            response.TryGetValue<RscpUInt8>(RscpTag.TAG_RSCP_AUTHENTICATION, out var authResponse);
-
-            var userLevel = (RscpUserLevel)authResponse.Value;
-        }
-    }
-}
+Console.ReadLine();
