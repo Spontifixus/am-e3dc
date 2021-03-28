080f73f4dfd425bd5a0d04ea771d05be86b5deba
Markus Rehrs <markus.rehrs@zuehlke.com>
2021-03-28 23:19:10 +0200

    RscpTime can now handle TimeStamps

--- Source/AM.E3dc.Rscp.Data/Values/RscpTime.cs
+++ Source/AM.E3dc.Rscp.Data/Values/RscpTime.cs
@@ -6,16 +6,30 @@ namespace AM.E3dc.Rscp.Data.Values
     /// <summary>
     /// Value object used to transport messages with an <see cref="RscpTimestamp"/> payload.
     /// </summary>
-    public sealed class RscpTime : RscpValue<DateTime>
+    public sealed class RscpTime : RscpValue
     {
+        private readonly RscpTimestamp timestamp;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="RscpTime"/> class.
         /// </summary>
         /// <param name="tag">The tag of the value object.</param>
-        /// <param name="value">The value of the object.</param>
-        public RscpTime(RscpTag tag, DateTime value)
-        : base(tag, 12, value)
+        /// <param name="value">The timespan to be represented by this object.</param>
+        public RscpTime(RscpTag tag, TimeSpan value)
+            : base(tag, (ushort)Marshal.SizeOf<RscpTimestamp>())
         {
+            this.timestamp = new RscpTimestamp(value);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RscpTime"/> class.
+        /// </summary>
+        /// <param name="tag">The tag of the value object.</param>
+        /// <param name="value">The date to be represented by this object.</param>
+        public RscpTime(RscpTag tag, DateTime value)
+            : base(tag, (ushort)Marshal.SizeOf<RscpTimestamp>())
+        {
+            this.timestamp = new RscpTimestamp(value);
         }
 
         /// <summary>
@@ -24,17 +38,28 @@ namespace AM.E3dc.Rscp.Data.Values
         /// <param name="tag">The tag of the value object.</param>
         /// <param name="data">The span containing the value of this object.</param>
         internal RscpTime(RscpTag tag, ReadOnlySpan<byte> data)
-            : base(tag, 12, MemoryMarshal.Read<RscpTimestamp>(data).ToDateTime())
+            : base(tag, (ushort)Marshal.SizeOf<RscpTimestamp>())
         {
+            this.timestamp = MemoryMarshal.Read<RscpTimestamp>(data);
         }
 
         /// <inheritdoc />
         public override RscpDataType DataType => RscpDataType.Timestamp;
 
+        /// <summary>
+        /// Gets the value of this instance represented as DateTime object.
+        /// </summary>
+        public DateTime DateTime => this.timestamp.ToDateTime();
+
+        /// <summary>
+        /// Gets the value of this instance represented as TimeSpan (since the unix epoch).
+        /// </summary>
+        public TimeSpan TimeSpan => this.timestamp.ToTimeSpan();
+
         private protected override void OnWrite(Span<byte> destination)
         {
-            var value = new RscpTimestamp(this.Value);
-            MemoryMarshal.Write(destination, ref value);
+            var tempTimestamp = this.timestamp;
+            MemoryMarshal.Write(destination, ref tempTimestamp);
         }
     }
 }
