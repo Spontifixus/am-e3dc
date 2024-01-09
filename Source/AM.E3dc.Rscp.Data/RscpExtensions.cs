e979d306cc805944fa912f5a2fb2715c80b04a8a
Markus Rehrs <github@markus.rehrs.de>
2024-01-09 21:11:14 +0100

    Added extension methods to easily access values

---
+++ Source/AM.E3dc.Rscp.Data/RscpExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using AM.E3dc.Rscp.Data.Values;
+
+namespace AM.E3dc.Rscp.Data
+{
+    /// <summary>
+    /// This class provides extensions for RSCP data classes.
+    /// </summary>
+    public static class RscpExtensions
+    {
+        /// <summary>
+        /// Recursively gets a value from an <see cref="RscpFrame" />.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="frame">The frame to be searched.</param>
+        /// <param name="tag">The requested tag.</param>
+        /// <returns>The requested value or null.</returns>
+        public static TValue Get<TValue>(this RscpFrame frame, RscpTag tag)
+        {
+            return frame.Values.Get<TValue>(tag);
+        }
+
+        /// <summary>
+        /// Recursively gets a value from a list or <see cref="RscpValue" />s.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="values">The values to be searched.</param>
+        /// <param name="tag">The requested tag.</param>
+        /// <returns>The requested value or null.</returns>
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration", Justification = "Reviewed.")]
+        public static TValue Get<TValue>(this IEnumerable<RscpValue> values, RscpTag tag)
+        {
+            var result = values
+                .Where(v => v.Tag == tag)
+                .OfType<TValue>()
+                .FirstOrDefault();
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            foreach (var value in values.OfType<RscpContainer>())
+            {
+                result = value.Get<TValue>(tag);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            return default;
+        }
+
+        /// <summary>
+        /// Recursively gets a value from an <see cref="RscpContainer" />.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="container">The container to be searched.</param>
+        /// <param name="tag">The requested tag.</param>
+        /// <returns>The requested value or null.</returns>
+        public static TValue Get<TValue>(this RscpContainer container, RscpTag tag)
+        {
+            return container.Children.Get<TValue>(tag);
+        }
+    }
+}
---
+++ Tests/AM.E3dc.Rscp.Data.Tests/RscpExtensionsFixture.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using AM.E3dc.Rscp.Data.Values;
+using FluentAssertions;
+using Xunit;
+
+namespace AM.E3dc.Rscp.Data.Tests
+{
+    public class RscpExtensionsFixture
+    {
+        private readonly RscpInt32 valueInContainer;
+        private readonly RscpInt8 valueOutsideOfContainer;
+        private readonly RscpFrame frame;
+
+        public RscpExtensionsFixture()
+        {
+            this.valueInContainer = new RscpInt32(RscpTag.BAT_CHARGE_CYCLES, 600);
+            this.valueOutsideOfContainer = new RscpInt8(RscpTag.BAT_INDEX, 0);
+            var container = new RscpContainer(RscpTag.BAT_DATA) { this.valueInContainer };
+            this.frame = new ()
+            {
+                this.valueOutsideOfContainer,
+                container
+            };
+        }
+
+        [Fact]
+        public void ReturnsNullIfValueTypeNotFound()
+        {
+            RscpValue result = null;
+            var action = new Action(
+                () =>
+                {
+                    result = this.frame.Get<RscpInt64>(RscpTag.BAT_CHARGE_CYCLES);
+                });
+
+            action.Should()
+                .NotThrow();
+            result.Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public void ReturnsNullIfRscpTagNotFound()
+        {
+            RscpValue result = null;
+            var action = new Action(
+                () =>
+                {
+                    result = this.frame.Get<RscpInt32>(RscpTag.EMS_PV_ENERGY);
+                });
+
+            action.Should()
+                .NotThrow();
+            result.Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public void CanRetrieveValueFromFrameThatIsNotInAContainer()
+        {
+            RscpValue result = null;
+            var action = new Action(
+                () =>
+                {
+                    result = this.frame.Get<RscpInt8>(RscpTag.BAT_INDEX);
+                });
+
+            action.Should().NotThrow();
+            result.Should().BeEquivalentTo(this.valueOutsideOfContainer);
+        }
+
+        [Fact]
+        public void CanRetrieveValueFromFrameThatIsInAContainer()
+        {
+            RscpValue result = null;
+            var action = new Action(
+                () =>
+                {
+                    result = this.frame.Get<RscpInt32>(RscpTag.BAT_CHARGE_CYCLES);
+                });
+
+            action.Should().NotThrow();
+            result.Should().BeEquivalentTo(this.valueInContainer);
+        }
+    }
+}
